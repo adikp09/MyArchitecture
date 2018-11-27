acf630b97976c35f3899bc5cab1088ee541510ac
adikp09 <adikp09>
2018-11-27 11:29:01 +0700

    update mvvm architecture

---
+++ app/src/main/java/com/dev/adi/myarchitecture/view_model/Main2Activity.java
@@ -0,0 +1,14 @@
+package com.dev.adi.myarchitecture.view_model;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.dev.adi.myarchitecture.R;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
