ce3b29a6171f0b81bf66a6fab9b98d5635b109d1
adikp09 <adikp09>
2018-11-28 11:25:27 +0700

    create mvc game

--- app/src/main/java/com/dev/adi/myarchitecture/controller/Main2Activity.java
+++ app/src/main/java/com/dev/adi/myarchitecture/controller/Main2Activity.java
@@ -2,6 +2,7 @@ package com.dev.adi.myarchitecture.controller;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -9,11 +10,16 @@ import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.TextView;
 import com.dev.adi.myarchitecture.R;
+import com.dev.adi.myarchitecture.model.Board;
+import com.dev.adi.myarchitecture.model.Player;
 
 public class Main2Activity extends AppCompatActivity {
 
+    private Board model;
+
     private Button button;
     private TextView textView;
+    private View textViewGrup;
     private GridLayout gridLayout;
 
     @Override
@@ -22,27 +28,42 @@ public class Main2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
         textView = findViewById(R.id.textView);
+        textViewGrup = findViewById(R.id.textViewGrup1);
         gridLayout = findViewById(R.id.gridLayout);
+
+        model = new Board();
     }
 
     public void onClickBtn(View v) {
         button = (Button) v;
 
-        String buttonTag = (String) button.getTag();
-        String row, col;
-        row = buttonTag.substring(0,1);
-        col = buttonTag.substring(1,2);
+        String tag = (String) button.getTag();
+        int row = Integer.valueOf(tag.substring(0,1));
+        int col = Integer.valueOf(tag.substring(1,2));
+        Log.i("this", "Row: [" + row + "," + col + "]");
 
-        button.setText(row + ", " + col);
+        Player move = model.setBoardValue(row, col);
 
-//        Intent intent;
-//        intent = new Intent(this, WinnerActivity.class);
-//        //send data
-//        intent.putExtra("data", "You Win");
-//        startActivity(intent);
+        if (move != null) {
+            button.setText(move.toString());
+            if (model.getIsWinner() != null) {
+                textView.setText(move.toString());
+                textViewGrup.setVisibility(View.VISIBLE);
+            }
+        }
 
-        textView.setText(row + ", " + col);
     }
+
+    public void resetGame() {
+        textViewGrup.setVisibility(View.GONE);
+        textView.setText("");
+
+        model.reset();
+        for (int i = 0; i < gridLayout.getChildCount(); i++) {
+            ((Button) gridLayout.getChildAt(i)).setText("");
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -53,10 +74,7 @@ public class Main2Activity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.action_new_game) {
-            int childCount = gridLayout.getChildCount();
-            for (int i = 0; i < childCount; i++) {
-                ((Button) gridLayout.getChildAt(i)).setText("");
-            }
+            resetGame();
         }
 
         return super.onOptionsItemSelected(item);
--- app/src/main/java/com/dev/adi/myarchitecture/model/Board.java
+++ app/src/main/java/com/dev/adi/myarchitecture/model/Board.java
@@ -1,4 +1,108 @@
 package com.dev.adi.myarchitecture.model;
 
+import android.util.Log;
+
+import static com.dev.adi.myarchitecture.model.Player.O;
+import static com.dev.adi.myarchitecture.model.Player.X;
+
 public class Board {
+
+    private Boolean isOver;
+    private Player turnPlayer;
+    private Player isWinner;
+
+    private Cell cell[][] = new Cell[3][3];
+
+    public Board() {
+        reset();
+    }
+
+    public void clearCellValue() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                cell[i][j] = new Cell();
+            }
+        }
+    }
+
+    public void reset() {
+        clearCellValue();
+        isWinner = null;
+        isOver = false;
+        turnPlayer = X;
+    }
+
+    public Player getIsWinner() {
+        return isWinner;
+    }
+
+    public boolean checkGame(int row, int col) {
+        if (isOver) {
+            return false;
+        } else if (isAlreadySet(row, col)) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean isAlreadySet(int row, int col) {
+        return cell[row][col].getCell() != null;
+    }
+
+
+    public Player setBoardValue(int row, int col) {
+        Player move = null;
+        if (checkGame(row, col)) {
+            cell[row][col].setCell(turnPlayer);
+            move = turnPlayer;
+            if (checkWin(turnPlayer, row, col)) {
+                isOver = true;
+                isWinner = turnPlayer;
+            } else {
+                changeTurn();
+            }
+        }
+        return move;
+    }
+
+    public boolean isDraw() {
+        Log.e("isDraw", "1");
+        Boolean isDraw = true;
+        for (int i = 0; i < 3; i++) {
+            Log.e("i", "2");
+            for (int j = 0; j < 3; j++) {
+                Log.e("j", "3");
+                if (cell[i][j].getCell() == null) {
+                    Log.e("isDraw", turnPlayer.toString());
+                    isDraw = false;
+                }
+            }
+        }
+        return isDraw;
+    }
+
+    public boolean checkWin(Player player, int row, int col) {
+        return (cell[row][0].getCell() == player
+                && cell[row][1].getCell() == player
+                && cell[row][2].getCell() == player
+
+                || cell[0][col].getCell() == player
+                && cell[1][col].getCell() == player
+                && cell[2][col].getCell() == player
+
+                || row == col
+                && cell[0][0].getCell() == player
+                && cell[1][1].getCell() == player
+                && cell[2][2].getCell() == player
+
+                || row + col == 2
+                && cell[0][0].getCell() == player
+                && cell[1][1].getCell() == player
+                && cell[2][2].getCell() == player
+        );
+    }
+
+    public void changeTurn() {
+        turnPlayer = turnPlayer == X ? O : X;
+    }
 }
